38bfefde0fdba9f26d312a5657ae92e772c8a94f
Noah Hensley <noahehensley@gmail.com>
2025-07-26 21:35:53 -0400

    Moved build scripts to /build dir.

--- build/build.bat
+++ build/build.bat
@@ -1,28 +1,23 @@
 @echo off
 rem Define variables
 set PYINSTALLER=pyinstaller
-set SCRIPT=src\main.py
-set OUTPUT_DIR=dist
+set SCRIPT=..\src\main.py
+set OUTPUT_DIR=..\dist
 set TEMP_DIR=temp
-set UTILS_DIR=%CD%\utils
-
+set UTILS_DIR=%CD%\..\utils
 rem Create temp directory for spec and build files
 if not exist %TEMP_DIR% mkdir %TEMP_DIR%
-
 rem Build the executable
 echo Building executable with PyInstaller...
 %PYINSTALLER% --onefile --add-data "%UTILS_DIR%;utils" %SCRIPT% --specpath %TEMP_DIR% --workpath %TEMP_DIR% --distpath %OUTPUT_DIR%
-
 rem Cleanup temp directory
 if exist %TEMP_DIR% (
     rmdir /s /q %TEMP_DIR%
 )
-
 rem Check if the build was successful
 if exist %OUTPUT_DIR% (
     echo Build completed successfully. Check the %OUTPUT_DIR% directory for the executable.
 ) else (
     echo Build failed. Please check for errors.
 )
-
 pause
