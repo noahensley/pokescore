2e61cd8f54d5928adf70a03e35942b10baab524b
Noah Hensley <noahehensley@gmail.com>
2025-08-04 20:23:21 -0400

    Added a startup print statement.

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import UIInfo
 # Main program
 if __name__ == "__main__":
     try:
+        print("Starting Pokescore Application...")
         ui = UIInfo.UIInfo()
     except RuntimeError as e:
         print(f"ERROR: {e}\nExiting...")
